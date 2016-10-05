644febaf47b447e8eac582a1ade1c3f0a739b0b1
Weylin Wagnon <weylinwagnon@gmail.com>
2016-10-04 18:25:01 -0700

    commit before reworking

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
 #!/usr/bin/env node --harmony
 
-var fs = require('fs');
-var https = require('https');
-var program = require('commander');
-var ipfsAPI = require('ipfs-api')();
+const fs = require('fs');
+const https = require('https');
+const program = require('commander');
+const ipfsAPI = require('ipfs-api')();
+const events = require('events');
+// const util = require('util');
+
+function Watcher(watchDir) {
+  this.watchDir = watchDir;
+}
+
+Watcher.prototype = new events.EventEmitter();
+Watcher.prototype.watch = () => {
+  fs.readdir(this.watchDir, (err, files) => {
+    if (err) console.error(err);
+    this.emit('hash', files);
+  });
+}
+const hashes = [];
+const hashFile = (file) => {
+  ipfsAPI.util.addFromFs(file, (err, result) => {
+    console.log('file inside hash file function: ', file);
+    if (err) {
+      throw err
+    }
+    let options = { host: "ipfs.io", path: `/ipfs/${result[0]["hash"]}` }
+    callback = (res) => {
+      let str = '';
+      res.on('data', chunk => {
+        str += chunk;
+      })
+      res.on('end', () => console.log(str));
+      res.on('error', (err) => {
+        console.error(err);
+      })
+    }
+    https.request(options, callback).end()
+  })
+}
+Watcher.on('hash', (files) => {
+  hashfile(files);
+})
 // var ipfsOG = require('ipfs')
 // var ProgressBar = require('progress');
 //   var chalk = require('chalk');
@@ -18,29 +56,11 @@ program
   // .option('-u, --username <user÷name>', 'The user to authenticate as')
   // .option('-p, --password <password>', 'The user\'s password')
   .action(function (file) {
-    // var ipfs = ipfsAPI('localhost', '5001', { protocol: 'http' }) // leaving out the arguments will default to these values 
+    // var ipfs = ipfsAPI('localhost', '5001', { protocol: 'http' }) 
     // ipfsOG
-    
 
-    ipfsAPI.util.addFromFs(file, (err, result) => {
-      if (err) {
-        throw err
-      }
-      let options = { host: "ipfs.io", path: "/ipfs/" + result[0]["hash"] }
-      console.log(options.path, options.hash);
-      callback = (res) => {
-        let str = '';
-        res.on('data', chunk => {
-          str += chunk;
-        })
-        res.on('end', () => console.log(str));
-        res.on('error', (err) => {
-          console.error(err);
-        })
-      }
-      https.request(options, callback).end();
-      console.log(result)
-    })
+
+    
   })
   .parse(process.argv);
 
